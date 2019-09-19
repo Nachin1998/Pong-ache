961d6cc4e21ccfaae09964e57a3947739dac2678
Nachin1998 <Ignacio.f.l@hotmail.com>
2019-09-19 20:34:07 -0300

    Tidier code, music broken

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -3,16 +3,25 @@
 const int screenWidth = 800;
 const int screenHeight = 450;
 const int playerMax = 2;
-const int ballMax = 20;
+const int ballMax = 10;
 const int win = 100;
+const Vector2 ballPositionInit = { (float)screenWidth / 2, (float)screenHeight / 2 };
 
+void menu();
 void game();
+void endScreen();
+void collisions(int &counterBall, int &counterColor, Color &background);
+void barrierShooting();
+void multiplyBall(int &counterBall);
+void changeColor(int &counterColor, Color &background);
 
 struct players {
 	Rectangle rec;
 	Vector2 size;
 	Color playerColor;
 	int points;
+	int ammo;
+	bool haveAmmo;
 }player[playerMax];
 
 struct barriers {
@@ -25,46 +34,17 @@ struct barriers {
 struct balls {
 	Vector2 ballSpeed;
 	Vector2 ballPosition;
+	int radius;
 	bool active;
 }ball[ballMax];
 
+struct ammunition {
+	Vector2 ammoSpeed;
+	Vector2 ammoPosition;
+	bool active;
+}ammo;
+
 Music music;
-
-void multiplyBall(int &counterBall) {
-	counterBall++;
-
-	if (counterBall <= ballMax) {
-		ball[counterBall].active = true;
-	}
-
-	for (int i = counterBall; i < ballMax; i++)
-	{
-		if (ball[i].active == true) {
-			ball[i].ballPosition = ball[0].ballPosition;
-			if (i % 2 == 0)
-				ball[i].ballSpeed.x *= -1.1f;
-			else
-				ball[i].ballSpeed.y *= -1.1f;
-		}
-	}
-}
-
-void changeColor(int &counterColor, Color &background) {
-	if (counterColor % 2 == 0) {
-		player[0].playerColor = BLACK;
-		player[1].playerColor = BLACK;
-		background = WHITE;
-	}
-	else {
-		player[0].playerColor = WHITE;
-		player[1].playerColor = WHITE;
-		background = BLACK;
-	}
-	if (counterColor >= 1) {
-		counterColor = 0 - 1;
-	}
-	counterColor++;
-}
 
 void endScreen() {
 
@@ -126,22 +106,17 @@ void menu() {
 }
 
 void game() {
-	//music = LoadMusicStream("Audio/Megalovania.ogg");
+	music = LoadMusicStream("Audio/Megalovania.ogg");
 
 	PlayMusicStream(music);
 
-	int ballRadius = 5;
 	Color background = BLACK;
 	int counterColor = 0;
 	int counterTiempo = 0;
 	int counterBall = 0;
 
-	//Balls
-	Vector2 playerSize = { (float)18, (float)120 };
-	Vector2 barrierSize = { (float)8, (float)120 };
-	Vector2 ballPositionInit = { (float)screenWidth / 2, (float)screenHeight / 2 };
-
 	//Players
+	Vector2 playerSize = { (float)18, (float)120 };
 	for (int i = 0; i < playerMax; i++)
 	{
 		player[i].rec.y = screenHeight / 2;
@@ -149,12 +124,15 @@ void game() {
 		player[i].rec.height = playerSize.y;
 		player[i].size = playerSize;
 		player[i].points = 0;
+		player[i].ammo = 0;
+		player[i].haveAmmo = false;
 		player[i].playerColor = WHITE;
 	}
 	player[0].rec.x = screenWidth - screenWidth + 30;
 	player[1].rec.x = screenWidth - 50;
 
 	//Barriers
+	Vector2 barrierSize = { (float)8, (float)120 };
 	for (int i = 0; i < playerMax; i++)
 	{
 		barrier[i].rec.width = barrierSize.x;
@@ -163,13 +141,13 @@ void game() {
 		barrier[i].active = false;	
 	}
 	
-
 	//Balls
 	for (int i = 0; i < ballMax; i++)
 	{
 		ball[i].ballPosition = ballPositionInit;
 		ball[i].ballSpeed.x = 5.0f;
 		ball[i].ballSpeed.y = 5.0f;
+		ball[i].radius = 5;
 		ball[i].active = false;
 	}
 	ball[0].active = true;
@@ -208,53 +186,10 @@ void game() {
 				ball[i].ballPosition.y += ball[i].ballSpeed.y;
 			}
 		}
+		
+		collisions(counterBall, counterColor, background);
 
-		//Detects the colision with the right scoring wall
-		for (int i = 0; i < ballMax; i++)
-		{
-			if (ball[i].ballPosition.x >= GetScreenWidth()) {
-				player[0].points++;
-				ball[i].ballPosition = ballPositionInit;
-				changeColor(counterColor, background);
-			}
-		}
-		//Detects the colision with the left scoring wall
-		for (int i = 0; i < ballMax; i++)
-		{
-			if (ball[i].ballPosition.x <= 0) {
-				player[1].points++;
-				ball[i].ballPosition = ballPositionInit;
-				changeColor(counterColor, background);
-			}
-		}
-
-		//Detects the colision with walls and changes the color
-		for (int i = 0; i < ballMax; i++)
-		{
-			if ((ball[i].ballPosition.y >= (GetScreenHeight() - ballRadius)) || (ball[i].ballPosition.y <= ballRadius)) {
-				ball[i].ballSpeed.y *= -1.0f;
-				changeColor(counterColor, background);
-			}
-		}
-
-		//Detects the colision with player and changes the color
-		for (int i = 0; i < ballMax; i++)
-		{
-			if (CheckCollisionCircleRec(ball[i].ballPosition, ballRadius, player[0].rec) ||
-				CheckCollisionCircleRec(ball[i].ballPosition, ballRadius, player[1].rec)) {
-
-				ball[i].ballSpeed.x *= -1.0f;
-				if (ball[i].ballSpeed.x < 0) {
-					ball[i].ballPosition.x -= 15;
-					changeColor(counterColor, background);
-				}
-				else {
-					ball[i].ballPosition.x += 15;
-					changeColor(counterColor, background);
-				}
-			}
-		}
-
+		//Goes to the end screen
 		if (player[0].points >= win || player[1].points >= win) {
 			endScreen();
 		}
@@ -271,60 +206,141 @@ void game() {
 		}
 		DrawRectangleRec(player[0].rec, player[0].playerColor);
 		DrawRectangleRec(player[1].rec, player[1].playerColor);
-		DrawRectangleRec(barrier[0].rec, player[0].playerColor);
-		DrawRectangleRec(barrier[1].rec, player[0].playerColor);
-		DrawText(FormatText("%i", player[0].points), 340, 200, 50, player[0].playerColor);
-		DrawText(FormatText("%i", player[1].points), 420, 200, 50, player[1].playerColor);
-
-		for (int i = 0; i < ballMax; i++)
+		for (int i = 0; i < playerMax; i++)
 		{
-			if (CheckCollisionCircleRec(ball[i].ballPosition, ballRadius, barrier[0].rec) ||
-				CheckCollisionCircleRec(ball[i].ballPosition, ballRadius, barrier[1].rec)) {
-				ball[i].ballSpeed.x *= -1.0f;
-				multiplyBall(counterBall);
-				if (ball[i].ballSpeed.x < 0) {
-					ball[i].ballPosition.x -= 15;
-					changeColor(counterColor, background);
-				}
-				else {
-					ball[i].ballPosition.x += 15;
-					changeColor(counterColor, background);
-				}
-				
+			if (barrier[i].active == true)
+			{
+				DrawRectangleRec(barrier[i].rec, player[i].playerColor);
 			}
 		}
+		DrawText(FormatText("%i", player[0].points), 340, 200, 50, player[0].playerColor);
+		DrawText(FormatText("%i", player[1].points), 425, 200, 50, player[1].playerColor);
 
-		if (IsKeyPressed(KEY_SPACE)) {
-			barrier[0].active = true;
-		}
-		if (barrier[0].active == true) {
-			barrier[0].rec.x += 5;
-
-			if (barrier[0].rec.x > 390) {
-				barrier[0].active = false;
-			}
-		}
-		if (barrier[0].active == false) {
-			barrier[0].rec.x = player[0].rec.x+10;
-			barrier[0].rec.y = player[0].rec.y;
-		}
-
-		if (IsKeyPressed(KEY_RIGHT_CONTROL)) {
-			barrier[1].active = true;
-		}
-		if (barrier[1].active == true) {
-			barrier[1].rec.x -= 5;
-
-			if (barrier[1].rec.x < 400) {
-				barrier[1].active = false;
-			}
-		}
-		if (barrier[1].active == false) {
-			barrier[1].rec.x = player[1].rec.x;
-			barrier[1].rec.y = player[1].rec.y;
-		}
+		barrierShooting();
 		EndDrawing();
 	}
+}
+
+void collisions(int &counterBall, int &counterColor, Color &background) {
+	for (int i = 0; i < ballMax; i++)
+	{
+		//Detects the colision with player and changes the color
+		if (CheckCollisionCircleRec(ball[i].ballPosition, ball[i].radius, player[0].rec) ||
+			CheckCollisionCircleRec(ball[i].ballPosition, ball[i].radius, player[1].rec)) {
+
+			ball[i].ballSpeed.x *= -1.0f;
+			if (ball[i].ballSpeed.x < 0) {
+				ball[i].ballPosition.x -= 15;
+				changeColor(counterColor, background);
+			}
+			else {
+				ball[i].ballPosition.x += 15;
+				changeColor(counterColor, background);
+			}
+		}
+
+		//Detects the colision with shot barriers and changes the color
+		if (CheckCollisionCircleRec(ball[i].ballPosition, ball[i].radius, barrier[0].rec) ||
+			CheckCollisionCircleRec(ball[i].ballPosition, ball[i].radius, barrier[1].rec)) {
+			ball[i].ballSpeed.x *= -1.0f;
+			multiplyBall(counterBall);
+			if (ball[i].ballSpeed.x < 0) {
+				ball[i].ballPosition.x -= 15;
+				changeColor(counterColor, background);
+			}
+			else {
+				ball[i].ballPosition.x += 15;
+				changeColor(counterColor, background);
+			}
+		}
+
+		//Detects the colision with walls and changes the color
+		if ((ball[i].ballPosition.y >= (GetScreenHeight() - ball[i].radius)) || (ball[i].ballPosition.y <= ball[i].radius)) {
+			ball[i].ballSpeed.y *= -1.0f;
+			changeColor(counterColor, background);
+		}
+
+		//Detects the colision with the left scoring wall
+		if (ball[i].ballPosition.x <= 0) {
+			player[1].points++;
+			ball[i].ballPosition = ballPositionInit;
+			changeColor(counterColor, background);
+		}
+
+		//Detects the colision with the right scoring wall
+		if (ball[i].ballPosition.x >= GetScreenWidth()) {
+			player[0].points++;
+			ball[i].ballPosition.x *= 1.5f;
+			ball[i].ballPosition = ballPositionInit;
+			changeColor(counterColor, background);
+		}
+	}
+}
+
+void barrierShooting() {
+	if (IsKeyPressed(KEY_SPACE)) {
+		barrier[0].active = true;
+	}
+	if (barrier[0].active == true) {
+		barrier[0].rec.x += 5;
+	}
+	if (barrier[0].rec.x > 390) {
+		barrier[0].active = false;
+	}
+	if (barrier[0].active == false) {
+		barrier[0].rec.x = player[0].rec.x + 10;
+		barrier[0].rec.y = player[0].rec.y;
+	}
+
+	if (IsKeyPressed(KEY_RIGHT_CONTROL)) {
+		barrier[1].active = true;
+	}
+	if (barrier[1].active == true) {
+		barrier[1].rec.x -= 5;
+	}
+	if (barrier[1].rec.x < 400) {
+		barrier[1].active = false;
+	}
+	if (barrier[1].active == false) {
+		barrier[1].rec.x = player[1].rec.x;
+		barrier[1].rec.y = player[1].rec.y;
+	}
+}
+
+void multiplyBall(int &counterBall) {
+	counterBall++;
+
+	if (counterBall <= ballMax) {
+		ball[counterBall].active = true;
+	}
+
+	for (int i = counterBall; i < ballMax; i++)
+	{
+		if (ball[i].active == true) {
+			ball[i].ballPosition = ball[0].ballPosition;
+			if (i % 2 == 0)
+				ball[i].ballSpeed.x *= -1.1f;
+			else
+				ball[i].ballSpeed.y *= -1.1f;
+		}
+	}
+}
+
+void changeColor(int &counterColor, Color &background) {
+	if (counterColor % 2 == 0) {
+		player[0].playerColor = BLACK;
+		player[1].playerColor = BLACK;
+		background = WHITE;
+	}
+	else {
+		player[0].playerColor = WHITE;
+		player[1].playerColor = WHITE;
+		background = BLACK;
+	}
+	if (counterColor >= 1) {
+		counterColor = 0 - 1;
+	}
+	counterColor++;
 }
 
 void main()
